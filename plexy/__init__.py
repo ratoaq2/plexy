7729fea8bc08603552d652f2cf421dde0e8f8351
Rato <ratoaq2@users.noreply.github.com>
2024-06-23 08:33:54 +0200

    release 0.1.4

--- plexy/__init__.py
+++ plexy/__init__.py
@@ -5,7 +5,7 @@ __version__ = metadata.version(__package__)
 __short_version__ = '.'.join(__version__.split('.')[:2])
 __author__ = metadata.metadata(__package__)['author']
 __license__ = metadata.metadata(__package__)['license']
-__url__ = metadata.metadata(__package__)['home_page']
+__url__ = metadata.metadata(__package__).get('home_page', 'https://github.com/ratoaq2/plexy')
 
 del metadata
 
